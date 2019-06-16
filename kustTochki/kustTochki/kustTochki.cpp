2c4d8eaa0c3259c51c5915573b62a69a8c200f0a
texasrodeo <49993352+texasrodeo@users.noreply.github.com>
2019-06-16 14:08:45 +0300

    Update kustTochki.cpp

--- kustTochki/kustTochki/kustTochki.cpp
+++ kustTochki/kustTochki/kustTochki.cpp
@@ -10,6 +10,8 @@ struct tochka {
 	double x, y;
 };
 
+
+
 void vvod(tochka *a, int n) {
 	for (int i = 0; i < n; i++) {
 		a[i].name = (char)((int)'A' + rand() % 26);
@@ -18,39 +20,85 @@ void vvod(tochka *a, int n) {
 	}
 }
 
+void vvod2(tochka *a, int n) {
+	double X, Y;
+	for (int i = 0; i < n; i++) {
+		cin >> X;
+		a[i].x = X;
+		cin >> Y;
+		a[i].y = Y;
+	}
+}
+
 void vivod(tochka *a, int n) {
 	for (int i = 0; i < n; i++) {
 		cout << a[i].name << " " << "(" << a[i].x << ";" << a[i].y << ")" << "   ";
 	}
+	cout << endl;
 }
 
 void vivodTreh(tochka a, tochka b, tochka c) {
 	cout << a.name << " " << "(" << a.x << ";" << a.y << ")" << "   ";
 	cout << b.name << " " << "(" << b.x << ";" << b.y << ")" << "   ";
 	cout << c.name << " " << "(" << c.x << ";" << c.y << ")" << "   ";
-	cout << " не составляют треугольник"<<endl;
+	cout << " не составляют треугольник"<< endl;
+
+}
+
+void vivodTrehTreug(tochka a, tochka b, tochka c) {
+	cout << a.name << " " << "(" << a.x << ";" << a.y << ")" << "   ";
+	cout << b.name << " " << "(" << b.x << ";" << b.y << ")" << "   ";
+	cout << c.name << " " << "(" << c.x << ";" << c.y << ")" << "   ";
+	cout << " составляют треугольник" << endl;
 
 }
 
 void vivodPryamoi(double k, double b) {
-	cout << "Уравнение прямой: " << k << "*x+ " << b;
+	if (k == 1) {
+		if (b == 0) {
+			cout << "Уравнение прямой: y = " << "x"<< endl;
+		}
+		else
+		{
+			cout << "Уравнение прямой: y = " << "x + " << b << endl;
+		}
+		
+	}
+	else
+	{
+		if (k == 0) {
+			cout << "Уравнение прямой: y = " <<  b << endl;
+		}
+		else
+		{
+			if (b == 0) {
+				cout << "Уравнение прямой: y = " << k << "*x" << endl;
+			}
+			else
+			{
+				cout << "Уравнение прямой: y = " << k << "*x + " << b << endl;
+			}
+		}
+		
+	}
+	
 }
 
 bool treug(tochka a, tochka b, tochka c) {
-	double ab = sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y));
-	double ac = sqrt((a.x - c.x)*(a.x - c.x) + (a.y - c.y)*(a.y - c.y));
-	double bc = sqrt((c.x - b.x)*(c.x - b.x) + (c.y - b.y)*(c.y - b.y));
-	if (ab + bc < ac) {
+	double ab = floor(sqrt((a.x - b.x)*(a.x - b.x) + (a.y - b.y)*(a.y - b.y))*100+.5)/100;
+	double ac = floor(sqrt((a.x - c.x)*(a.x - c.x) + (a.y - c.y)*(a.y - c.y)) * 100 + .5) / 100;
+	double bc = floor(sqrt((c.x - b.x)*(c.x - b.x) + (c.y - b.y)*(c.y - b.y)) * 100 + .5) / 100;
+	if (ab + bc <= ac) {
 		return false;
 	}
 	else
 	{
-		if (ab + ac < bc) {
+		if (ab + ac <= bc) {
 			return false;
 		}
 		else
 		{
-			if (ac + bc < ac) {
+			if (ac + bc <= ab) {
 				return false;
 			}
 			else
@@ -62,33 +110,38 @@ bool treug(tochka a, tochka b, tochka c) {
 }
 
 double FindK(tochka a, tochka b) {
-	return (b.y - a.y) / (b.x - b.y);
+	return (b.y - a.y) / (b.x - a.x);
 }
 
 double FindB(tochka a, tochka b) {
-	return(b.x - a.y - a.x*b.y) / (b.x - a.x);
+	//return(b.x - a.y - a.x*b.y) / (b.x - a.x);
+	return b.y - (b.y - a.y) / (b.x - a.x)*b.x;
 }
 
 int main()
 {
 	setlocale(0, "rus");
-	const int n = 10;
+	const int n = 5;
 	tochka a[n];
-	vvod(a, n);
+	vvod2(a, n);
 	vivod(a, n);
 	for (int i = 0; i < n; i++)
 	{
-		for (int j = 0; j < n; j++) {
-			for (int l = 0; l < n; l++) {
+		for (int j = i; j < n; j++) {
+			for (int l = j; l < n; l++) {
 				if (i != j && i != l && j != l) {
 					if (!treug(a[i], a[j], a[l])) {
 						vivodTreh(a[i], a[j], a[l]);
 						vivodPryamoi(FindK(a[i], a[l]), FindB(a[i], a[l]));
 					}
+					else 
+					{
+						vivodTrehTreug(a[i], a[j], a[l]);
+					}
 				}
 			}
 		}
 	}
+	
 }
-
 
